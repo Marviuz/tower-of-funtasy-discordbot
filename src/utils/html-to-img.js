c68faa478045c7b921b828b0ae0cf75a14b4aa82
Marviuz <marviuz.gaming@gmail.com>
2022-10-13 14:06:17 +0800

    add transparent to `htmlToImg`

--- src/utils/html-to-img.js
+++ src/utils/html-to-img.js
@@ -3,6 +3,7 @@ const nodeHtmlToImage = require('node-html-to-image');
 const htmlToImg = async (template, data) => {
   const generatedImg = await nodeHtmlToImage({
     html: template,
+    transparent: true,
     content: { data }
   });
 
