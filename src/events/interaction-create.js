a902c24ffb8c960eebef3cc33ec6555e2b318c8c
Wizi8914 <loick.konieczny@gmail.com>
2022-10-19 00:53:56 +0200

    add rarity argument to the `/matrix` command

--- src/events/interaction-create.js
+++ src/events/interaction-create.js
@@ -1,9 +1,14 @@
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
-    if (!interaction.isChatInputCommand() || interaction.isButton()) return;
-
+    
     const command = interaction.client.commands.get(interaction.commandName);
+
+    if(interaction.isAutocomplete()) {
+      await command.autocomplete(interaction)
+    }
+
+    if (!interaction.isChatInputCommand() || interaction.isButton()) return;
 
     if (!command) return;
 
