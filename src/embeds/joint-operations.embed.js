a1856172da55f085eae18bdc18d5389eb4b44379
Wizi8914 <loick.konieczny@gmail.com>
2022-10-24 21:45:01 +0200

    fix `joint-operation` command

--- src/embeds/joint-operations.embed.js
+++ src/embeds/joint-operations.embed.js
@@ -1,12 +1,17 @@
 const { EmbedBuilder } = require('discord.js');
-const jointOperations = require('../db/joint-operations.json');
 const { emojis } = require('../utils/app-constants');
 
 module.exports = async (data) => {
   let date = data.timestamp[0]
+  let date2 = data.timestamp[1]
   let datemessage = "";
 
+
   if(new Date().getHours() >= 5 && (new Date().getDay()-1)%7 != new Date(data.timestamp[0] * 1000).getDay() || new Date().getHours() >= 5 && (new Date().getDay()-1)%7 != new Date(data.timestamp[1] * 1000).getDay()) {
+
+    if(new Date().getDay() === new Date(date * 1000).getDay()) {
+      date += 86400
+    }
 
     while (Math.round(Date.now() / 1000) > date) {
       date += 604800
@@ -22,16 +27,19 @@ module.exports = async (data) => {
 
     if (data.availability.includes(['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'][(new Date(Date.now()).getDay()-1)%7])) {
       datemessage = "(Available) Ends in:"
-      date = jointOperations.filter(_ => _.availability.includes(['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'][(new Date(date * 1000).getDay())]))[0].timestamp[0]
     } else {
       datemessage = "Available in:"
     }
 
   } else {
-    date -= 518400
+    date +=86400
+    while (Math.round(Date.now() / 1000) > date) {
+      date += 604800
+    }
+    date -= 86400
+
     datemessage = "(Available) Ends in:"
   }
-
 
   let res = ""
   data.resistance.forEach(element => {
@@ -42,14 +50,13 @@ module.exports = async (data) => {
       }
   });
 
-
   const embed = new EmbedBuilder()
     .setTitle(data.name)
 
     .addFields(
       { name: 'Availability:', value: data.availability.join(', ') },
       { name: 'Enemy Resistances:', value: res },
-      { name: datemessage, value: `<t:${date}:R>` }
+      { name: datemessage, value: `<t:${date}:R> ||(UTC+2)||` }
     )
     .setImage(data.img);
 
