f5ca9d040f6d7952865cae328117e3dad73d17b6
Marviuz <marviuz.gaming@gmail.com>
2022-10-26 22:47:15 +0800

    add new reminder for dailies and weeklies

--- src/embeds/reminder.embed.js
+++ src/embeds/reminder.embed.js
@@ -3,14 +3,38 @@ const { EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const fileName = 'tof-logo-alt.png';
 const attachment = new AttachmentBuilder(`src/assets/${fileName}`); // Might have problems with relative pathing
 
+const dailies = [
+  'Daily Bounty',
+  'Resin (Interstellar Exploration if they have better rewards)',
+  'Gifts',
+  'Traning',
+  'Weapon upgrade mats (Commisary > Weapon Store)',
+  'Daily Supply Box (Store)',
+  'Crew Donation',
+  'Sign-in',
+  'Claim Artificial Island Materials'
+].map(_ => `・${_}`).join('\n');
+
+const weeklies = [
+  'Booster module & Advancement module (Crystal Dust Store)',
+  'Weekly SSR Weapon (Weapon Store)',
+  'Black Nucleus, Joint Supply Chip I & Gold (Support Store)',
+  'Void Rift',
+  'Frontier Clash',
+  'Banges Item Vendor (Claude)',
+  'Claire\'s Dream Machine',
+  'Artifial Island Bosses',
+  'Raid'
+].map(_ => `・${_}`).join('\n');
+
 const ReminderEmbed = new EmbedBuilder()
   .setColor(0x0099FF)
   .setTitle('ToF Dailies & Weeklies')
   .setDescription('*Note: I might\'ve forgotten to add many things here*')
   .setThumbnail(`attachment://${fileName}`)
   .addFields(
-    { name: 'Dailies', value: '— Daily Bounty\n— Resin (Interstellar Exploration if they have better rewards)\n— Gifts\n— Traning\n— Weapon upgrade mats (Commisary > Weapon Store)\n— Daily Supply Box (Store)\n— Crew Donation\n— Sign-in' },
-    { name: 'Weeklies', value: '— Booster module & Advancement module (Crystal Dust Store)\n— Weekly SSR Weapon (Weapon Store)\n— Black Nucleus, Joint Supply Chip I & Gold (Support Store)\n— Void Rift\n— Frontier Clash\n— Banges Item Vendor (Claude)\n— Claire\'s Dream Machine' },
+    { name: 'Dailies', value: dailies },
+    { name: 'Weeklies', value: weeklies },
   )
   .setTimestamp();
 
