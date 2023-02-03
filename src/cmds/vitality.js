ab9e99f111eb9e01582809eee66590b1a950c06a
Wizi8914 <loick.konieczny@gmail.com>
2023-02-03 23:06:27 +0100

    update vitality command

--- src/cmds/vitality.js
+++ src/cmds/vitality.js
@@ -1,7 +1,6 @@
 const path = require('path');
-const { SlashCommandBuilder } = require('discord.js');
-const { emojis } = require('../utils/app-constants');
-const { getAllVitality } = require('../db/models/provider');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { emojis, RED } = require('../utils/app-constants');
 const { vitalitySchema } = require('../db/models/schema.vitality');
 const schedule = require('node-schedule');
 
@@ -17,19 +16,28 @@ module.exports = {
         .setDescription(DESCRIPTION)
         .addStringOption(option =>
             option.setName('current_vitality')
-              .setDescription('Your current level')
+              .setDescription('Your current Vitality')
               .setRequired(true)
         )
         .addStringOption(option =>
           option.setName('desired_vitality')
-            .setDescription('Your current critical')
+            .setDescription('The number of Vitality you want')
         ),
 
     async execute(interaction) {
-        const current = interaction.options.getString('current_vitality');
-        let desired = interaction.options.getString('desired_vitality');
-    
-        if(!desired) { desired = 180 }
+      const current = interaction.options.getString('current_vitality');
+      let desired = interaction.options.getString('desired_vitality');
+
+      if(!desired) { desired = 180 }
+
+      /* Errors Messages */
+      if (isNaN(Number(current))) {
+        return interaction.reply({ embeds: [{ color: RED, title: ':x: You must enter a number' }] })
+      } else if (current > desired) {
+        return interaction.reply({ embeds: [{ color: RED, title: ':x: The desired vitality cannot be lower than your current vitality' }] })
+      } else if (current > 180 || current < 0 || desired > 180 || desired < 0) {
+        return interaction.reply({ embeds: [{ color: RED, title: ':x: Your vitality and that desired must be contained between 0 and 180' }] })
+      }
 
         const timeMessage = Math.floor(Date.now() / 1000) + ((desired - current) * 480)
         
@@ -38,14 +46,26 @@ module.exports = {
           id: interaction.user.id,
           time: timeMessage
         }).save();
-        */
+        */        
 
         const date = Date.now() + 10 * 1000; // 5 minutes à partir de maintenant
+
+        const embed = new EmbedBuilder()
+            .setColor('Green')
+            .setTitle(':white_check_mark: Vitality timer successfully set')
+            .setDescription(`
+              **Current Vitality:** ${current} ${emojis["vitality"]} 
+              **Desired Vitality:** ${desired} ${emojis["vitality"]}
+
+              **A message will be sent to you in:**
+                <t:${Math.round(date / 1000)}:R> (<t:${Math.round(date / 1000)}:T>) 
+            `)
+            
 
         schedule.scheduleJob(new Date(date), () => {
           interaction.user.send(`${desired}`)
         });
 
-        interaction.reply(emojis["vitality"])
+        interaction.reply({ embeds: [embed] })
   },
 };
