871b3101aa8691fc509adcbc19f8667443ce09be
Wizi8914 <loick.konieczny@gmail.com>
2023-04-28 19:48:00 +0200

    start creating `team` command

---
+++ src/cmds/teams.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { SlashCommandBuilder } = require('discord.js');
+
+const NAME = path.parse(__filename).name;
+const DESCRIPTION = 'Allows giving meta teams according to different characteristics';
+
+module.exports = {
+  name: NAME,
+  description: DESCRIPTION,
+  type: 'tof',
+  data: new SlashCommandBuilder()
+    .setName(NAME)
+    .setDescription(DESCRIPTION)
+    .addStringOption(option =>
+        option.setName('role'),
+        option.setDescription('Role of the team'),
+    )
+    .addStringOption(option =>
+        option.setName('element'),
+        option.setDescription('Element of the team'),
+    ),
+
+  async execute(interaction) {
+    const role = interaction.options.getString('role');
+    const element = interaction.options.getString('element');
+    await interaction.reply(`${element} ${role}`);
+  },
+};
