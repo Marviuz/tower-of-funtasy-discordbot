05b3f5539491cada56e8c3d4f8acf7b39da431c1
Wizi8914 <loick.konieczny@gmail.com>
2023-05-29 22:44:18 +0200

    working on teams command

--- src/cmds/teams.js
+++ src/cmds/teams.js
@@ -1,8 +1,12 @@
 const path = require('path');
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const teamsData = require('../db/local/teams.json');
+const simulacraData = require('../db/local/charInfo.json');
+const { createCanvas, loadImage } = require('canvas');
 
 const NAME = path.parse(__filename).name;
 const DESCRIPTION = 'Allows giving meta teams according to different characteristics';
+
 
 module.exports = {
   name: NAME,
@@ -14,15 +18,69 @@ module.exports = {
     .addStringOption(option => option
       .setName('role')
       .setDescription('Role of the team')
+      .setRequired(true)
+      .addChoices(
+        { name: 'Attack', value: 'attack' },
+        { name: 'Fortitude', value: 'fortitude' },
+        { name: 'Benediction', value: 'benediction' },
+        { name: 'Balance', value: 'balance' },
+      )
     )
     .addStringOption(option => option
       .setName('element')
       .setDescription('Element of the team')
+      .setRequired(true)
+      .addChoices(
+        { name: 'Frost', value: 'frost' },
+        { name: 'Flame', value: 'flame' },
+        { name: 'Volt', value: 'volt' },
+        { name: 'Physical', value: 'physical' },
+        { name: 'Altered', value: 'altered' },
+        { name: 'Rainbow', value: 'rainbow'},
+        { name: 'None', value: 'none' },
+        { name: 'All', value: 'all' },
+      )
     ),
 
   async execute(interaction) {
     const role = interaction.options.getString('role');
     const element = interaction.options.getString('element');
-    await interaction.reply(`${element} ${role}`);
+
+    const teams = teamsData[element][role];
+
+
+
+    let attachments = [];
+
+    for (const team of teams) {
+
+
+      let canvas = createCanvas(1500, 600);
+      let context = canvas.getContext('2d');
+
+      let increment = 0;
+
+      
+      for (const simulacrum of team) {
+        //console.log(simulacraData[simulacrum].img);
+
+        let image = await loadImage(simulacraData[simulacrum].img);
+        context.drawImage(image, increment, 0, 500, 500);
+        context.fillText(simulacraData[simulacrum].name, increment, 500 + 50) ;
+
+        increment += 500;
+      }
+
+
+
+      let buffer = canvas.toBuffer();
+
+      let attachment = new AttachmentBuilder(buffer, 'image.png');
+
+      await interaction.reply({ files: [attachment] });
+
+    }
+
+    
   },
 };
